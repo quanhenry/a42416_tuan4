8db4d4555ca2673a22ef9f78e76c31745f340f9b
quanhenry <100461077+quanhenry@users.noreply.github.com>
2024-04-19 14:15:57 +0700

    Create t4Contact.kt

---
+++ t4Contact.kt
@@ -0,0 +1,13 @@
+package com.example.a42416_tuan4.tuan4
+
+class T4Contact {
+    var ten: String?=null
+    var tuoi: String?= null
+    var hinh: Int=0
+    constructor()
+    constructor(ten:String?,tuoi:String?,hinh:Int){
+        this.ten=ten
+        this.tuoi=tuoi
+        this.hinh=hinh
+    }
+}
